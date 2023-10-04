178954fc97c3b77e68dc9187ad9f85ac7f9d3903
Mehakb77 <b7mehak@gmail.com>
2023-10-04 08:59:33 +0530

    updated home page details

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -3,38 +3,56 @@ import { Link } from 'react-router-dom';
 
 import ContactIcons from '../Contact/ContactIcons';
 
+import details from '../../data/details.json';
+
 const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+
+const { author, education, jobs } = details;
 
 const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/me.jpeg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <h2>{author.name}</h2>
+        <p>
+          <a href={`mailto:${author.mail}`}>{author.mail}</a>
+        </p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
+      <p>
+        Hi, I&apos;m {author.name}. I am a{' '}
+        <a href={education.link}>{education.university}</a> graduate and
+        {' '}{jobs[0].position} at{' '}
+        <a href={`${jobs[0].link}`}>{' '}{jobs[0].company}</a>. Previously, worked as{' '}
+        {jobs[1].position} at {' '}
+        <a href={`${jobs[0].link}`}>{' '}{jobs[1].company}</a>.
       </p>
       <ul className="actions">
         <li>
-          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+          {!window.location.pathname.includes('/resume') ? (
+            <Link to="/resume" className="button">
+              Learn More
+            </Link>
+          ) : (
+            <Link to="/about" className="button">
+              About Me
+            </Link>
+          )}
         </li>
       </ul>
     </section>
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      {/* <p className="copyright">
+        &copy; {} D&apos;Angelo <Link to="/">mldangelo.com</Link>.
+      </p> */}
     </section>
   </section>
 );
